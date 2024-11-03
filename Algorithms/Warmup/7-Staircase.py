5a526daeb535a58d59b0d57551d80a3bd669bbbd
Ahmad Nani <phantom.an95g@gmail.com>
2024-11-03 19:00:33 +0200

    Mini-Max Sum

--- Algorithms/Warmup/7-Staircase.py
+++ Algorithms/Warmup/7-Staircase.py
@@ -43,9 +43,18 @@
 # Solution:
 def staircase(n):
     for i in range(1, n + 1):
-        print((" " * (n - i)) + ("#" * i))
+        print(f'{"#"*i:>{n}}')
 
 if __name__ == '__main__':
     n = int(input().strip())
 
+    staircase(n)
+
+# or
+def staircase(n):
+    # Write your code here
+    for i in range(1, n + 1):
+        print((" " * (n - i)) + ("#" * i))
+if __name__ == '__main__':
+    n = int(input().strip())
     staircase(n)
